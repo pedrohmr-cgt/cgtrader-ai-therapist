f77e55ffaa40f66a7ec008ab2498ea61ebd5af32
Pedro Rodrigues <pedro.henrique@cgtrader.com>
2021-07-01 13:17:35 -0300

    General flow

--- src/pages/Session/Session.jsx
+++ src/pages/Session/Session.jsx
@@ -1,4 +1,5 @@
-import React, { useEffect } from "react";
+/* eslint-disable no-unused-vars */
+import React, { useEffect, useState } from "react";
 import { Button } from "antd";
 import * as faceapi from "face-api.js";
 
@@ -6,7 +7,14 @@ import Header from "../../components/Header/Header";
 
 import "./Session.scss";
 
+const STATUS_WAITING = 0;
+const STATUS_LISTENING = 1;
+const STATUS_PROCESSING = 2;
+const STATUS_DONE = 3;
+
 const Welcome = () => {
+  const [status, setStatus] = useState(STATUS_WAITING);
+
   function isFaceDetectionModelLoaded() {
     return !!faceapi.nets.tinyFaceDetector.params;
   }
@@ -49,9 +57,128 @@ const Welcome = () => {
     return true;
   }
 
-  async function collectData() {
-    await faceapi.loadFaceExpressionModel("/");
-    onPlay();
+  function clearData() {
+    setStatus(STATUS_WAITING);
+  }
+
+  function collectData() {
+    setStatus(STATUS_LISTENING);
+    // await faceapi.loadFaceExpressionModel("/");
+    // onPlay();
+  }
+
+  function processData() {
+    setStatus(STATUS_PROCESSING);
+    setTimeout(() => {
+      setStatus(STATUS_DONE);
+    }, 1000);
+
+    return true;
+  }
+
+  function renderStartButton() {
+    return (
+      <Button
+        onClick={collectData}
+        type="primary"
+        shape="round"
+        size="large"
+        block
+      >
+        Start my CGTherapy
+      </Button>
+    );
+  }
+
+  function renderListeningBox() {
+    return (
+      <div className="session__box">
+        <div className="session__box-listening">
+          Listening: <span>06 sec</span>
+        </div>
+        <div className="session__box-answer">
+          Please answer while looking at the camera
+        </div>
+        <h2 className="session__box-question">How do you feel right now?</h2>
+        <Button
+          onClick={processData}
+          type="ghost"
+          shape="round"
+          size="large"
+          block
+        >
+          Stop Session
+        </Button>
+      </div>
+    );
+  }
+
+  function renderProcessingBox() {
+    return (
+      <div className="session__box">
+        <div className="session__box-listening session__box-processing">
+          Listening: <span>Done</span>
+        </div>
+        <div className="session__box-answer">
+          We are processing your therapy session
+        </div>
+        <h2 className="session__box-question">89% Processed...</h2>
+      </div>
+    );
+  }
+
+  function renderResultBox() {
+    return (
+      <div className="session__box">
+        <p className="session__box-paragraph">Condition:</p>
+        <div className="session__box-emoji">&#x1F603;</div>
+        <p className="session__box-paragraph">Tips for well being:</p>
+        <ul className="session__box-tips">
+          <li>
+            Breathe in slowly. Deep breaths come from the diaphragm, not the
+            chest. It may help to visualize your breath rising from deep in your
+            belly.
+          </li>
+          <li>
+            Hold it. Hold your breath for a count of three, then let it out
+            slowly.
+          </li>
+          <li>
+            Consider a mantra. Some people find it helpful to repeat a mantra,
+            like &ldquo;I am calm&rdquo; or &ldquo;I am relaxed.&rdquo;
+          </li>
+          <li>
+            Consider a mantra. Some people find it helpful to repeat a mantra,
+            like &ldquo;I am calm&rdquo; or &ldquo;I am relaxed.&rdquo;
+          </li>
+        </ul>
+        <Button
+          onClick={clearData}
+          type="primary"
+          shape="round"
+          size="large"
+          block
+        >
+          Start another CGTherapy
+        </Button>
+      </div>
+    );
+  }
+
+  function renderBox() {
+    switch (status) {
+      case STATUS_LISTENING:
+        return renderListeningBox();
+
+      case STATUS_PROCESSING:
+        return renderProcessingBox();
+
+      case STATUS_DONE:
+        return renderResultBox();
+
+      default:
+        return renderStartButton();
+    }
   }
 
   useEffect(() => {
@@ -74,17 +201,7 @@ const Welcome = () => {
       <div className="session">
         {/* eslint-disable-next-line jsx-a11y/media-has-caption */}
         <video className="session__video" autoPlay />
-        <div className="session__box">
-          <Button
-            onClick={collectData}
-            type="primary"
-            shape="round"
-            size="large"
-            block
-          >
-            Start my CGTherapy
-          </Button>
-        </div>
+        <div className="session__container">{renderBox()}</div>
       </div>
     </>
   );
