6c407d78ba3b4232c9ce1f5d1203f03210435263
Pedro Rodrigues <pedro.henrique@cgtrader.com>
2021-07-01 14:08:27 -0300

    Refinements

--- src/pages/Session/Session.jsx
+++ src/pages/Session/Session.jsx
@@ -3,6 +3,7 @@ import React, { useEffect, useState } from "react";
 import { Button } from "antd";
 import * as faceapi from "face-api.js";
 
+import moods from "../../data/moods";
 import Header from "../../components/Header/Header";
 
 import "./Session.scss";
@@ -12,8 +13,17 @@ const STATUS_LISTENING = 1;
 const STATUS_PROCESSING = 2;
 const STATUS_DONE = 3;
 
+const MOOD_NEUTRAL = "neutral";
+const MOOD_HAPPY = "happy";
+const MOOD_SAD = "sad";
+const MOOD_FEARFUL = "fearful";
+const MOOD_DISGUESTED = "disgusted";
+const MOOD_SURPRISED = "surprised";
+
 const Welcome = () => {
   const [status, setStatus] = useState(STATUS_WAITING);
+  const [processing, setProcessing] = useState(0);
+  const [mood, setMood] = useState(null);
 
   function isFaceDetectionModelLoaded() {
     return !!faceapi.nets.tinyFaceDetector.params;
@@ -67,11 +77,36 @@ const Welcome = () => {
     // onPlay();
   }
 
+  function shuffleArray(array) {
+    const newArray = [...array];
+    // eslint-disable-next-line no-plusplus
+    for (let i = newArray.length - 1; i > 0; i--) {
+      const j = Math.floor(Math.random() * (i + 1));
+      const temp = newArray[i];
+      newArray[i] = newArray[j];
+      newArray[j] = temp;
+    }
+
+    return newArray;
+  }
+
+  function incrementProcessing() {
+    const processingIncrease = Math.round(Math.random() * 30);
+    if (processing + processingIncrease >= 100) {
+      const shuffleMood = shuffleArray(Object.keys(moods));
+      setStatus(STATUS_DONE);
+      setMood(shuffleMood[0]);
+      setProcessing(0);
+    } else {
+      setProcessing(processing + processingIncrease);
+    }
+
+    return true;
+  }
+
   function processData() {
     setStatus(STATUS_PROCESSING);
-    setTimeout(() => {
-      setStatus(STATUS_DONE);
-    }, 1000);
+    incrementProcessing();
 
     return true;
   }
@@ -89,13 +124,11 @@ const Welcome = () => {
       </Button>
     );
   }
-
+  // : <span>06 sec</span>
   function renderListeningBox() {
     return (
       <div className="session__box">
-        <div className="session__box-listening">
-          Listening: <span>06 sec</span>
-        </div>
+        <div className="session__box-listening">Listening...</div>
         <div className="session__box-answer">
           Please answer while looking at the camera
         </div>
@@ -122,36 +155,51 @@ const Welcome = () => {
         <div className="session__box-answer">
           We are processing your therapy session
         </div>
-        <h2 className="session__box-question">89% Processed...</h2>
+        <h2 className="session__box-question">{processing}% Processed...</h2>
       </div>
     );
+  }
+
+  function getMoodList() {
+    if (!moods[mood]) {
+      return null;
+    }
+
+    return moods[mood].map((_mood) => <li>{_mood}</li>);
+  }
+
+  function getMoodIcon() {
+    switch (mood) {
+      case "neutral":
+        return "1F610";
+
+      case "sad":
+        return "1F614";
+
+      case "angry":
+        return "1F621";
+
+      case "fearful":
+        return "1F628";
+
+      case "disgusted":
+        return "1F922";
+
+      case "surprised":
+        return "1F631";
+
+      default:
+        return "1F603";
+    }
   }
 
   function renderResultBox() {
     return (
       <div className="session__box">
         <p className="session__box-paragraph">Condition:</p>
-        <div className="session__box-emoji">&#x1F603;</div>
+        <div className="session__box-emoji">&#x1F641;</div>
         <p className="session__box-paragraph">Tips for well being:</p>
-        <ul className="session__box-tips">
-          <li>
-            Breathe in slowly. Deep breaths come from the diaphragm, not the
-            chest. It may help to visualize your breath rising from deep in your
-            belly.
-          </li>
-          <li>
-            Hold it. Hold your breath for a count of three, then let it out
-            slowly.
-          </li>
-          <li>
-            Consider a mantra. Some people find it helpful to repeat a mantra,
-            like &ldquo;I am calm&rdquo; or &ldquo;I am relaxed.&rdquo;
-          </li>
-          <li>
-            Consider a mantra. Some people find it helpful to repeat a mantra,
-            like &ldquo;I am calm&rdquo; or &ldquo;I am relaxed.&rdquo;
-          </li>
-        </ul>
+        <ul className="session__box-tips">{getMoodList()}</ul>
         <Button
           onClick={clearData}
           type="primary"
@@ -194,6 +242,13 @@ const Welcome = () => {
         });
     }
   }, []);
+
+  useEffect(() => {
+    if (processing > 0) {
+      setTimeout(incrementProcessing, 500);
+    }
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [processing]);
 
   return (
     <>
