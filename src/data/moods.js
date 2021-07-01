0534885d9bdfbb913b43bd97e311a69eaf044c67
Olegs Boldirevs <olegs.boldirevs@gmail.com>
2021-07-01 15:48:06 +0300

    Push moods

--- src/data/moods.js
+++ src/data/moods.js
@@ -1,11 +1,16 @@
 const moods = {
-  neutral: ["", ""],
-  happy: ["", ""],
-  sad: ["", ""],
-  angry: ["", ""],
-  fearful: ["", ""],
-  disgusted: ["", ""],
-  surprised: ["", ""],
+  neutral: ["It seems that work is not giving you joy at this moment - you might need some inspiration or just a good Joke", ""],
+  happy: ["You are having Fun! Or using some drugs which is it? In any case share it with your team :)", ""],
+  sad: ["https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRW7YRNOfVqdzuD0O73aDHw5SkCW1GsCyNgEmISF5IH5wOl0oK_V2Kvn6Vm3sQKq7P0fEg&usqp=CAU", ""],
+  angry: ["Take a deep breath!\n" +
+  "Breathe in slowly. Deep breaths come from the diaphragm, not the chest. It may help to visualize your breath rising from deep in your belly.\n" +
+  "Hold it. Hold your breath for a count of three, then let it out slowly.\n" +
+  "Consider a mantra. Some people find it helpful to repeat a mantra, like “I am calm” or “I am relaxed.”\n" +
+  "\n" +
+  "Talk to CGTherapist longer -> 60s session", "Go for a walk and clear your mind. It is okay to take a break sometimes."],
+  fearful: ["What is that scares you now?", ""],
+  disgusted: ["Why? Why you not like us?", ""],
+  surprised: ["Do not be surprised - things happen and this is not a last time :) Make the best of it", ""],
 };
 
 export default moods;
